1c5b194491b9aa84d448d05ce1bedccb21c89918
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-09 08:48:39 -0400

    Correccion y pruebas

--- src/main/kotlin/com/ucb/edu/abc/mscompany/api/AccountPlanApi.kt
+++ src/main/kotlin/com/ucb/edu/abc/mscompany/api/AccountPlanApi.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/config/enterprise/account")
+@RequestMapping("/config/enterprise/accountable-plan")
 class AccountPlanApi @Autowired constructor(
     private val accountBl: AccountBl
 ){
