e5dd0fd4acc2d99b71f2f8d81bcc89a3e55cc365
EmersonChipana <emerson.chipana@ucb.edu.bo>
2023-11-02 16:00:11 -0400

    Generar PDF

--- src/main/kotlin/com/ucb/edu/abc/mscompany/api/JournalApi.kt
+++ src/main/kotlin/com/ucb/edu/abc/mscompany/api/JournalApi.kt
@@ -1,18 +1,22 @@
 package com.ucb.edu.abc.mscompany.api
 
 import com.ucb.edu.abc.mscompany.bl.JournalBl
+import com.ucb.edu.abc.mscompany.bl.PDFTurtleBl
 import com.ucb.edu.abc.mscompany.dto.request.JournalRequestDto
 import com.ucb.edu.abc.mscompany.dto.response.JournalResponseDto
 import com.ucb.edu.abc.mscompany.dto.response.ResponseDto
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.nio.charset.Charset
 
 @RestController
 @RequestMapping("/diary/book")
-class JournalApi @Autowired constructor
-(private val journalBl: JournalBl)
-{
+class JournalApi @Autowired constructor(
+    private val journalBl: JournalBl,
+    private val pdfTurtleBl: PDFTurtleBl
+){
     @PostMapping("/{companyId}")
     fun getJournal(@PathVariable companyId: Int, @RequestBody journalRequestDto: JournalRequestDto): ResponseEntity<ResponseDto<JournalResponseDto>>{
         val journalResponseDto = journalBl.getJournal(companyId, journalRequestDto)
@@ -27,4 +31,19 @@ class JournalApi @Autowired constructor
 
         }
     }
+
+    @PostMapping("/pdf/{companyId}")
+    suspend fun getJournalPDF(@PathVariable companyId: Int, @RequestBody journalRequestDto: JournalRequestDto): ResponseEntity<ResponseDto<ByteArray>>{
+        val journalResponseDto = journalBl.getJournal(companyId, journalRequestDto)
+        val footer = pdfTurtleBl.readHtmlToString("ReportPDF/libro-diario/footer.html")
+        val header = pdfTurtleBl.readHtmlToString("ReportPDF/libro-diario/header.html")
+        val body = pdfTurtleBl.readHtmlToString("ReportPDF/libro-diario/index.html")
+        val pdf = pdfTurtleBl.getPDF(header, body, footer, journalResponseDto)
+        return ResponseEntity.ok(
+            ResponseDto(pdf, "Datos obtenidos con exito", true, "" ))
+    }
+
+
+
+
 }
